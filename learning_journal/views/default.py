94911f093870b49ee5a04f9ae486a84b8f090a0e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-01 17:30:32 -0700

    Updated views.

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,25 +1,25 @@
-from pyramid.response import Response
+from pyramid.view import view_config
 import os
 
 HERE = os.path.abspath(__file__)
 TEMPLATE = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'templates')
 
 
+@view_config(route_name='home', renderer='templates/index.jinja2')
 def list_view(request):
-    with open(os.path.join(TEMPLATE, 'index.html')) as file:
-        return Response(file.read())
+    return {}
 
 
+@view_config(route_name='post', renderer='templates/read.jinja2')
 def detail_view(request):
-    with open(os.path.join(TEMPLATE, 'read.html')) as file:
-        return Response(file.read())
+    return {}
 
 
+@view_config(route_name='new-entry', renderer='templates/create.jinja2')
 def create_view(request):
-    with open(os.path.join(TEMPLATE, 'create.html')) as file:
-        return Response(file.read())
+    return {}
 
 
+@view_config(route_name='edit-entry', renderer='templates/edit.jinja2')
 def update_view(request):
-    with open(os.path.join(TEMPLATE, 'edit.html')) as file:
-        return Response(file.read())
+    return {}
